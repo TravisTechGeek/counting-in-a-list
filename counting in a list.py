a2284c813028e5b9acb6f4437dabc783e377b3f4
Travis Paul Pyle <travispyle@gmail.com>
2025-07-21 16:28:56 -0400

    Add files via upload

---
+++ counting in a list.py
@@ -0,0 +1,7 @@
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Mrs. Wilson’s class is voting for class president. She has saved each student’s vote into the list votes.
+# Use .count() to determine how many students voted for "Jake" and save the value to a variable called jake_votes.
+jake_votes = votes.count("Jake")
+# Use print() to examine jake_votes.
+print(jake_votes)
